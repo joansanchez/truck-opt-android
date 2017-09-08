667d2392f9bbd77af9e42cbc0f2bc603854e9ad9
polvallsortiz <pol.valls.ortiz@est.fib.upc.edu>
2017-09-08 22:43:10 +0200

    MainActivity to IntroActivity

--- app/src/main/java/com/example/joan/truck_opt/MainActivity.java
+++ app/src/main/java/com/example/joan/truck_opt/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.joan.truck_opt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import agency.tango.materialintroscreen.MaterialIntroActivity;
+import agency.tango.materialintroscreen.MessageButtonBehaviour;
+import agency.tango.materialintroscreen.SlideFragmentBuilder;
+
+public class MainActivity extends MaterialIntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        addSlide(new SlideFragmentBuilder()
+                        .backgroundColor(R.color.colorPrimary)
+                        .buttonsColor(R.color.colorAccent)
+                        .image(agency.tango.materialintroscreen.R.drawable.ic_next)
+                        .title("title 3")
+                        .description("Description 3")
+                        .build(),
+                new MessageButtonBehaviour(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showMessage("We provide solutions to make you love your work");
+                    }
+                }, "Work with love"));
     }
 }
