de4fdb6dd28cddff49a3e6d3e3bc153fa8c04834
Joan Sanchez <jsanchezgarcia13@gmail.com>
2017-09-08 23:31:11 +0200

    login screen

--- app/src/main/java/com/example/joan/truck_opt/LoginActivity.java
+++ app/src/main/java/com/example/joan/truck_opt/LoginActivity.java
@@ -17,7 +17,7 @@ import android.widget.Toast;
 import butterknife.ButterKnife;
 import butterknife.Bind;
 
-public class login extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
 
