18cd01dec84b2297343241332882c143689644a4
polvallsortiz <pol.valls.ortiz@est.fib.upc.edu>
2017-09-08 23:47:33 +0200

    Colors

--- app/src/main/java/com/example/joan/truck_opt/IntroActivity.java
+++ app/src/main/java/com/example/joan/truck_opt/IntroActivity.java
@@ -1,31 +1,81 @@
 package com.example.joan.truck_opt;
 
+import android.Manifest;
+import android.support.annotation.FloatRange;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import agency.tango.materialintroscreen.MaterialIntroActivity;
 import agency.tango.materialintroscreen.MessageButtonBehaviour;
 import agency.tango.materialintroscreen.SlideFragmentBuilder;
+import agency.tango.materialintroscreen.animations.IViewTranslation;
 
 public class IntroActivity extends MaterialIntroActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            enableLastSlideAlphaExitTransition(true);
 
-        addSlide(new SlideFragmentBuilder()
-                        .backgroundColor(R.color.colorPrimary)
-                        .buttonsColor(R.color.colorAccent)
-                        .image(agency.tango.materialintroscreen.R.drawable.ic_next)
-                        .title("title 3")
-                        .description("Description 3")
-                        .build(),
-                new MessageButtonBehaviour(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        showMessage("We provide solutions to make you love your work");
-                    }
-                }, "Work with love"));
-    }
+            getBackButtonTranslationWrapper()
+                    .setEnterTranslation(new IViewTranslation() {
+                        @Override
+                        public void translate(View view, @FloatRange(from = 0, to = 1.0) float percentage) {
+                            view.setAlpha(percentage);
+                        }
+                    });
+
+            addSlide(new SlideFragmentBuilder()
+                            .backgroundColor(R.color.first_slide_background)
+                            .buttonsColor(R.color.first_slide_buttons)
+                            .image(R.drawable.ic_delivery_truck)
+                            .title("TruckU")
+                            .description("Optimizing the truck deliveries")
+                            .build(),
+                    new MessageButtonBehaviour(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            showMessage("We provide solutions to make you love your work");
+                        }
+                    }, "Work with love"));
+
+            addSlide(new SlideFragmentBuilder()
+                    .backgroundColor(R.color.first_slide_background)
+                    .buttonsColor(R.color.first_slide_buttons)
+                    .title("Want more?")
+                    .description("Go on")
+                    .build());
+        
+            addSlide(new SlideFragmentBuilder()
+                            .backgroundColor(R.color.first_slide_background)
+                            .buttonsColor(R.color.first_slide_buttons)
+                            .possiblePermissions(new String[]{Manifest.permission.CALL_PHONE, Manifest.permission.READ_SMS})
+                            .neededPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+                            .image(R.drawable.ic_delivery_truck)
+                            .title("We provide best tools")
+                            .description("ever")
+                            .build(),
+                    new MessageButtonBehaviour(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            showMessage("Try us!");
+                        }
+                    }, "Tools"));
+
+            addSlide(new SlideFragmentBuilder()
+                    .backgroundColor(R.color.first_slide_background)
+                    .buttonsColor(R.color.first_slide_buttons)
+                    .title("That's it")
+                    .description("Would you join us?")
+                    .build());
+        }
+
+        @Override
+        public void onFinish() {
+            super.onFinish();
+            Toast.makeText(this, "Try this library in your project! :)", Toast.LENGTH_SHORT).show();
+        }
 }
