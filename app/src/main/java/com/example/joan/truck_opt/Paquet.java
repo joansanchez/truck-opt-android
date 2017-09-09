a2ea23961ed78dc867c3ebe9cb14e068b96c56f6
Joan Sanchez <jsanchezgarcia13@gmail.com>
2017-09-09 17:30:59 +0200

    main activities

--- app/src/main/java/com/example/joan/truck_opt/Paquet.java
+++ app/src/main/java/com/example/joan/truck_opt/Paquet.java
@@ -1,9 +1,30 @@
 package com.example.joan.truck_opt;
 
+import java.util.ArrayList;
+
 /**
  * Created by joan on 9/09/17.
  */
 
 public class Paquet {
+    private int id;
+    private String from;
+    private String to;
+    private ArrayList<String> notis;
+
+    Paquet (int id, String from, String to, ArrayList<String> notis){
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.notis = new ArrayList<String>(notis);
+    }
+
+    public int getId () { return id;}
+
+    public String getFrom() {return from;}
+
+    public String getTo() {return to;}
+
+    public ArrayList<String> getnotis() { return notis;}
 
 }
