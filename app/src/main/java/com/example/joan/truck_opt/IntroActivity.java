d428750a1e0a7db9145bcaa8e80ff0e59c3c09de
polvallsortiz <pol.valls.ortiz@est.fib.upc.edu>
2017-09-09 09:21:00 +0200

    Intro

--- app/src/main/java/com/example/joan/truck_opt/IntroActivity.java
+++ app/src/main/java/com/example/joan/truck_opt/IntroActivity.java
@@ -3,7 +3,6 @@ package com.example.joan.truck_opt;
 import android.Manifest;
 import android.support.annotation.FloatRange;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -34,25 +33,27 @@ public class IntroActivity extends MaterialIntroActivity {
                             .image(R.drawable.logowidth)
                             .title("TruckU")
                             .description("Optimizing the truck deliveries")
-                            .build(),
-                    new MessageButtonBehaviour(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            showMessage("We provide solutions to make you love your work");
-                        }
-                    }, "Work with love"));
+                            .build());
 
             addSlide(new SlideFragmentBuilder()
-                    .backgroundColor(R.color.first_slide_background)
-                    .buttonsColor(R.color.first_slide_buttons)
-                    .title("Want more?")
-                    .description("Go on")
-                    .build());
+                    .backgroundColor(R.color.secondprimary)
+                    .buttonsColor(R.color.secondicons)
+                    .image(R.drawable.introactivitysecondslide)
+                    .title("Truck drivers welcome!")
+                    .description("App only available for professional truck drivers")
+                    .build(),
+                new MessageButtonBehaviour(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showMessage("Enter to our platform trucku.com");
+                    }
+                }, "What if I want to send?"));
+
         
             addSlide(new SlideFragmentBuilder()
                             .backgroundColor(R.color.first_slide_background)
                             .buttonsColor(R.color.first_slide_buttons)
-                            .neededPermissions(new String[]{Manifest.permission.ACCESS_NETWORK_STATE,Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+                            .neededPermissions(new String[]{Manifest.permission.INTERNET,Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
                             .image(R.drawable.ic_delivery_truck)
                             .title("We provide best tools")
                             .description("ever")
