c233fbfd5512561ab41585d64d216fc57d6e8cf6
Joan Sanchez <jsanchezgarcia13@gmail.com>
2017-09-09 15:43:50 +0200

    splash screen

--- app/src/main/java/com/example/joan/truck_opt/SplashScreen.java
+++ app/src/main/java/com/example/joan/truck_opt/SplashScreen.java
@@ -3,9 +3,12 @@ package com.example.joan.truck_opt;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
+import static android.R.attr.delay;
 
 public class SplashScreen extends AppCompatActivity {
     private static final String TAG = "SplashActivity";
@@ -15,25 +18,36 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.splash_screen);
+
         final Intent i = new Intent(this, IntroActivity.class);
         final Intent l = new Intent(this, LoginActivity.class);
         final Intent p = new Intent(this, MainActivity.class);
         sp = getSharedPreferences("APP2", Context.MODE_PRIVATE);
         editor = sp.edit();
-        Boolean firstuse = sp.getBoolean("firstuse",true);
+        final Boolean firstuse = sp.getBoolean("firstuse",true);
         String actual = sp.getString("currentUser",null);
-        if (firstuse) {
-            editor.putBoolean("firstuse", false);
-            editor.apply();
-            startActivity(i);
-        }
-        else {
-            if (sp.getString("currentUser", "").length() != 0) startActivity(p);
-            else{
-                startActivity(l);
-            }
 
-        }
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 5s = 5000ms
+                if (firstuse) {
+                    editor.putBoolean("firstuse", false);
+                    editor.apply();
+                    startActivity(i);
+                }
+                else {
+                    if (sp.getString("currentUser", "").length() != 0) startActivity(p);
+                    else{
+                        startActivity(l);
+                    }
+
+                }
+            }
+        }, 1500);
+
+
     }
 }
